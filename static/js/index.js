21bf4ccfb66e391f8e3a3987bd045ee4d5425e5b
Daniel Ryaboshapka <dan4821@gmail.com>
2020-11-12 13:25:58 -0500

    add initial SimpleArm translation

--- static/js/index.js
+++ static/js/index.js
@@ -41,10 +41,12 @@ import {
   PerspectiveCamera,
   Scene,
   WebGLRenderer,
+  HemisphereLight,
   MOUSE
 } from './three.module.js';
 
 import { OrbitControls } from './OrbitControls.js';
+import { STLLoader } from './STLLoader.js';
 
 const container = document.querySelector('#container');
 const scene = new Scene();
@@ -65,8 +67,9 @@ const material = new MeshBasicMaterial();
 const cube = new Mesh(geometry, material);
 
 scene.add(cube);
+scene.add(new HemisphereLight(0xffffff, 1.5));
 
-const renderer = new WebGLRenderer();
+const renderer = new WebGLRenderer({ antialias: true, alpha: true });
 
 renderer.setSize(container.clientWidth, container.clientHeight);
 
@@ -75,10 +78,13 @@ renderer.setPixelRatio(window.devicePixelRatio);
 container.append(renderer.domElement);
 
 renderer.render(scene, camera);
-const controls = new OrbitControls( camera, renderer.domElement );
-controls.mouseButtons = { LEFT: MOUSE.ROTATE, MIDDLE: MOUSE.DOLLY, RIGHT: MOUSE.PAN }
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.rotateSpeed = 0.05;
+controls.dampingFactor = 0.1;
+controls.enableZoom = true;
 controls.autoRotate = true;
-controls.autoRotateSpeed = 1;
+controls.autoRotateSpeed = .75;
 function animate() {
  
     requestAnimationFrame(animate);
@@ -89,3 +95,159 @@ function animate() {
 };
 
 animate();
+
+// QUATERNION
+// const quaternion = new THREE.Quaternion();
+// quaternion.setFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), Math.PI / 2 );
+
+// const vector = new THREE.Vector3( 1, 0, 0 );
+// vector.applyQuaternion( quaternion );
+
+// EULER
+// const a = new THREE.Euler( 0, 1, 1.57, 'XYZ' );
+// const b = new THREE.Vector3( 1, 0, 1 );
+// b.applyEuler(a);
+
+// def quatsFromJointLimits(jlims):
+// 	quats = []
+// 	for min_max, axis in jlims:
+// 		lower = min_max[0]
+// 		upper = min_max[1]
+// 		temp = []
+// 		temp.append(axisAngleToQuat(axis, lower))
+// 		temp.append(axisAngleToQuat(axis, upper))
+// 		quats.append(temp)
+
+// 	return quats
+
+// def distanceBetween(j1, j2): 
+// 	return abs(np.linalg.norm(j1-j2))
+
+class SimpleArm {
+	constructor(nodes, edges, jlims, anglelims, axes){
+		this.nodes = nodes;
+		this.edges = edges;
+		this.jlims = jlims; 
+		this.anglelims = anglelims;
+		this.axes  = axes;
+		this.distances = new Array(nodes.length);
+	}
+
+	checkDistances(){
+		return true; 
+	}
+
+	updateDistances(){
+		return true;
+	}
+
+	addNodes(){ 
+		return true;
+	}
+
+	addEdges(){
+		return true;
+	}
+
+	currRot(){
+		return true;
+	}
+
+	debugging(){
+		return true; 
+	}
+
+	maxDistance(){
+		return 10; 
+	}
+
+	num_nodes(){
+		return this.node.length; 
+	}
+
+	num_links(){
+		return this.edges.length;
+	}
+
+	setAngles(){
+
+	}
+
+	getAngle(index){
+		return this.anglelims[index];
+	}
+
+	getEulerAngles(){
+		return this.anglelims;
+	}
+
+	getAxis(index){
+		return this.axes[index];
+	}
+
+	getEndEffector(){
+		return this.nodes.slice(-1)[0];
+	}
+
+	getDistanceOfJointLink(index){
+
+	}
+
+	setNode(index, node){
+
+	}
+
+	constrainQuat(quaternion, motor_index){
+
+	}
+
+	rotateJByQuat(index, q){
+
+	}
+
+	isQuatWithinBounds(q_check, q_range){
+
+	}
+
+
+	checkBounds(j_to_rotate, motor_index, target){
+
+	}
+
+}
+
+// function CCDIK(s, t, tolerance, steps){
+// 	let ctr = 0
+	
+// 	// TODO
+// 	let dist = distanceBetween(s.getEndEffector(), t)
+
+// 	while (dist > tolerance && ctr < steps){
+// 		for (let i=s.num_links(); i >= 0; i--){
+
+// 			let curr = s.getNode(i)
+// 			let ee   = s.getEndEffector()
+
+// 			let vec_to_ee = ee - curr
+// 			let vec_to_target = t - curr
+
+// 			let quat_rot = findQuatforVecs(vec_to_ee, vec_to_target)
+// 			let quat_inv = quat_rot.inverse()
+// 			let curr_axis = s.getAxis(i)
+
+// 			let parent_axis = rotatePByQuat(curr_axis, quat_inv)
+// 			let quat_axis = findQuatforVecs(curr_axis, parent_axis)
+// 			let new_quat = quat_rot.multiply(quat_axis)
+
+// 			let new_q = s.constrainQuat(new_quat, i)
+// 			if (!new_q.equals(Quaternion(0,0,0,0))){
+// 				s.rotateJByQuat(i, new_quat)
+// 			}
+// 		}
+
+// 		dist = distanceBetween(s.getEndEffector(), t)
+// 		ctr += 1
+// 	}
+
+// 	return ctr
+// }
